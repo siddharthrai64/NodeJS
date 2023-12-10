e776abd8242676727fcbd9955ec7d8e3e094865c
siddharthrai64 <siddharthrai64@gmail.com>
2023-12-10 10:18:10 +0530

    .

--- LearningCodes/AsynchronousCodes/Files&fs/app.js
+++ LearningCodes/AsynchronousCodes/Files&fs/app.js
@@ -1,3 +1,17 @@
 var fs = require('fs');
-var greet = fs.readFileSync(__dirname, './greet', 'utf8');
+
+// Synchronous Execution -> First File reading Completed then moves to console statement
+var greet = fs.readFileSync(__dirname + '/greet.txt', 'utf8');
 console.log(greet);
+// Hello World
+
+
+// Asynchronous Execution -> File reading is happening asynchronously and moves to console statement directly
+var greet2 = fs.readFile(__dirname + '/greet.txt', 'utf8', (err, data) => {
+    console.log(data);
+});
+
+console.log('Done'); 
+// Hello World ! -> Synchronous
+// Done
+// Hello World ! -> Asynchronous
